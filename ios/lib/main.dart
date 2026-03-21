3abb0108fc3b4ac7382ba8833667a70ba13dae98
Giovanni Gemignani <giovanni.gemignani@outlook.com>
2026-03-21 20:56:30 -0300

    commit1

--- ios/lib/main.dart
+++ ios/lib/main.dart
@@ -1,121 +1,169 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return const MaterialApp(home: MyHomePage());
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final TextEditingController _controller = TextEditingController();
+  String _nomeProduto = "";
+  double _sliderValue = 0.0;
+  String? _selectedRadio = '';
+  String _dropdownValue = 'Norte';
+  bool _isCheckboxChecked = false;
+  String _resumo = "";
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Olá Flutter'), centerTitle: true),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Preencha com o nome do produto:'),
+
+                const SizedBox(height: 20),
+
+                TextField(
+                  controller: _controller,
+                  decoration: const InputDecoration(
+                    labelText: 'Digite o nome do produto',
+                    border: OutlineInputBorder(),
+                  ),
+                  onChanged: (valor) {
+                    setState(() {
+                      _nomeProduto = valor;
+                    });
+                  },
+                ),
+
+                const SizedBox(height: 30),
+
+                Text("Defina a Quantidade: ${_sliderValue.round()}"),
+
+                Slider(
+                  value: _sliderValue,
+                  min: 0,
+                  max: 100,
+                  divisions: 100,
+                  label: _sliderValue.round().toString(),
+                  onChanged: (double val) {
+                    setState(() {
+                      _sliderValue = val;
+                    });
+                  },
+                ),
+
+                const Text("Escolha o tipo de entrega:"),
+                Row(
+                  children: [
+                    Radio<String>(
+                      value: 'A',
+                      groupValue: _selectedRadio,
+                      onChanged: (val) => setState(() => _selectedRadio = val),
+                    ),
+                    const Text("Carreto"),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Radio<String>(
+                      value: 'B',
+                      groupValue: _selectedRadio,
+                      onChanged: (val) => setState(() => _selectedRadio = val),
+                    ),
+                    const Text("Retirada"),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Radio<String>(
+                      value: 'C',
+                      groupValue: _selectedRadio,
+                      onChanged: (val) => setState(() => _selectedRadio = val),
+                    ),
+                    const Text("Correio"),
+                  ],
+                ),
+                const Text("DropdownButton:"),
+                DropdownButton<String>(
+                  value: _dropdownValue,
+                  onChanged: (String? newValue) {
+                    setState(() => _dropdownValue = newValue!);
+                  },
+                  items: <String>['Norte', 'Sul', 'Leste', 'Oeste']
+                      .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      })
+                      .toList(),
+                ),
+
+                const Text("Deseja receber promoções via e-mail?"),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Checkbox(
+                      value: _isCheckboxChecked,
+                      onChanged: (bool? value) {
+                        setState(() => _isCheckboxChecked = value!);
+                      },
+                    ),
+                    const Text("Sim, desejo receber promoções via e-mail."),
+                  ],
+                ),
+
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      String tipoEntregaTexto = "";
+
+                      if (_selectedRadio == 'A') tipoEntregaTexto = "Carreto";
+                      if (_selectedRadio == 'B') tipoEntregaTexto = "Retirada";
+                      if (_selectedRadio == 'C') tipoEntregaTexto = "Correio";
+
+                      _resumo =
+                          "O produto selecionado foi: $_nomeProduto, "
+                          "mas a quantidade de produtos escolhida foi ${_sliderValue.round()}. "
+                          "O pedido será entregue via $tipoEntregaTexto. "
+                          "A região de entrega será $_dropdownValue. "
+                          "O cliente aceita o envio de promoções: "
+                          "${_isCheckboxChecked ? "Aceito" : "Não aceita"}";
+                    });
+                  },
+                  child: const Text('Cadastrar'),
+                ),
+
+                const SizedBox(height: 20),
+
+                Text(_resumo, textAlign: TextAlign.center),
+              ],
             ),
-          ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
